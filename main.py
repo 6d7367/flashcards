04cfffccedb7f80270e835c16cca9bd6e4d1a411
6d7367 <6d7367@gmail.com>
2017-12-05 00:04:19 +0200

    QuestionMode

--- main.py
+++ main.py
@@ -89,6 +89,92 @@ class FlashCardsMode(Mode):
 				self.show_next()
 				time.sleep(5)
 
+class QuestionMode(Mode):
+	def start(self, root):
+		question_frame = tk.Frame(root)
+		choices_frame = tk.Frame(root)
+		next_frame = tk.Frame(root)
+
+		style_question = ("Helvetica", 29)
+		style_choice = ("Helvetica", 18)
+
+		self.question_var = tk.StringVar(root)
+		self.question_var.set("?")
+		question_label = tk.Label(question_frame, textvar = self.question_var , font = style_question)
+
+		self.answer_variable = tk.IntVar()
+		self.choices = {}
+
+		self.default_color = "#000"
+		self.wrong_color = "#f00"
+		self.right_color = "#0c0"
+
+		for i in range(4):
+			choice_text_var = tk.StringVar()
+			choice_text_var.set("choice {}".format(i))
+			self.choices[i] = {
+				"text_var": choice_text_var,
+				"radio": tk.Radiobutton(choices_frame, fg = self.default_color, textvar = choice_text_var, value = i, variable = self.answer_variable, command = self._check_answer, font = style_choice),
+				"is_true": False,
+			}
+			self.choices[i]["radio"].pack(anchor = tk.W)
+
+		tk.Button(next_frame, text = ">>", command = self.show_next).pack(pady = 10)
+
+
+		question_label.pack(fill = tk.BOTH)
+
+		question_frame.pack(side = tk.TOP, expand = 1)
+		choices_frame.pack(side = tk.TOP, expand = 1)
+		next_frame.pack(side = tk.BOTTOM, fill = tk.X)
+
+		self.show_next()
+
+	def show_next(self, *args, **kwargs):
+		self._default_choices_colors()
+
+		new_choices = self._next_questions()
+
+		for k, choice in new_choices["choices"].items():
+			choice_text = self.data[choice]["translated"]
+			self.choices[k]["text_var"].set(choice_text)
+			self.choices[k]["is_true"] = False
+
+			if k == new_choices["right_answer"]:
+				question_text = self.data[choice]["original"]
+				self.question_var.set(question_text)
+				self.choices[k]["is_true"] = True
+	
+	def _default_choices_colors(self):
+		for _, choice in self.choices.items():
+			choice["radio"].configure(fg = self.default_color)
+
+	def _next_questions(self):
+		new_choices_indexes = {}
+		while True:
+			if len(new_choices_indexes) == 4:
+				break
+
+			word_index = random.randint(0, self.data_len - 1)
+			if word_index not in new_choices_indexes.values():
+				new_choices_indexes[len(new_choices_indexes)] = word_index
+
+		right_answer_index = random.randint(0, 3)
+
+		return { "choices": new_choices_indexes, "right_answer": right_answer_index }
+
+	def _check_answer(self):
+		self._default_choices_colors()
+		answer = self.answer_variable.get()
+		for k, choice in self.choices.items():
+			if k == answer:
+				if not choice["is_true"]:
+					choice["radio"].configure(fg = self.wrong_color)
+
+			if choice["is_true"]:
+				choice["radio"].configure(fg = self.right_color)
+
+
 
 
 class App(tk.Tk):
@@ -102,7 +188,7 @@ class App(tk.Tk):
 		self.mode.start(self)
 
 if __name__ == '__main__':
-	mode = FlashCardsMode(DEFAULT_DATA_FILE)
+	mode = QuestionMode(DEFAULT_DATA_FILE)
 	app = App(mode)
 
 	app.mainloop()
